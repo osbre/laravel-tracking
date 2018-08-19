578a367522a2cb1eb90c2321c01ac14fea567921
ostap <ostap34php@gmail.com>
2018-08-19 16:38:50 +0300

    bug with dates

--- app/Track.php
+++ app/Track.php
@@ -9,30 +9,42 @@ class Track extends Model
 {
     protected $fillable = ['code', 'from', 'to', 'load', 'dims', 'at_origin', 'at_origin_date', 'freight_loaded', 'freight_loaded_date', 'current_location', 'current_location_date', 'at_distination', 'at_distination_date', 'delivered', 'status', 'pod'];
 
-    protected $dates = ['at_origin_date', 'freight_loaded_date', 'current_location_date', 'at_distination_date', 'delivered'];
+    //protected $dates = ['at_origin_date', 'freight_loaded_date', 'current_location_date', 'at_distination_date', 'delivered'];
 
     public function setAtOriginDateAttribute($value)
     {
-        $this->attributes['at_origin_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        if (!empty($value)) {
+            $this->attributes['at_origin_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        }
     }
 
     public function setFreightLoadedDateAttribute($value)
     {
-        $this->attributes['freight_loaded_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        if (!empty($value)) {
+            $this->attributes['freight_loaded_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        }
     }
 
     public function setCurrentLocationDateAttribute($value)
     {
-        $this->attributes['current_location_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        if (!empty($value)) {
+            $this->attributes['current_location_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        }
+
     }
 
     public function setAtDistinationDateAttribute($value)
     {
-        $this->attributes['at_distination_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        if (!empty($value)) {
+            $this->attributes['at_distination_date'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        }
+
     }
 
     public function setDeliveredAttribute($value)
     {
-        $this->attributes['delivered'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        if (!empty($value)) {
+            $this->attributes['delivered'] = Carbon::createFromFormat('m-d-Y H:i', $value);
+        }
     }
 }
